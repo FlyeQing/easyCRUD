84ed7544c41d57c7fd7d78df2e1df6a8028b94d1
FlyeQing <2423568178@qq.com>
2016-05-26 21:18:43 +0800

    PHP+MySQL简易的增删改查操作

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 #easyCRUD
 简单的增删改查，学习PHP+MySQL: <a href="http://www.w3school.com.cn/sql/sql_syntax.asp">sql语法</a>
 
+###1.插入记录：INSERT INTO table_name (列1, 列2,...) VALUES (值1, 值2,....)
+```sql
+mysql> INSERT tb1 VALUES('TOM',25,5000);//插入一条记录：tom，25岁，工资5000
+Query OK, 1 row affected
+```
--- post.php
+++ post.php
@@ -1,42 +1,40 @@
 <?php
 include_once('connect.php');
-$action = $_POST['action']?$_POST['action']:$_GET['action'];
-switch($action){
+$action = $_POST['action'] ? $_POST['action'] : $_GET['action'];
+switch ($action) {
     case 'add':  //新增项
-	   $title = htmlspecialchars($_POST['title']);
-	   $query = mysql_query("insert into catalist (cid,title) values (NULL,'$title')");
-	   if($query){
-		   $insertid = mysql_insert_id($link);
-		   $arr = array('id'=>$insertid,'title'=>$title,'success'=>1);
-	   }else{
-		   $arr = array('success'=>2);
-	   }
-	   echo json_encode($arr);
-	   break;
-	case 'del':  //删除项
-	   $id = $_POST['id'];
-	   $query = mysql_query("delete from catalist where cid=".$id);
-	   if($query){
-		   echo '1';
-	   }else{
-		   echo '2';
-	   }
-	   break;
-	case 'edit':  //编辑项
-	   $id = $_POST['id'];
-	   $title = $_POST['title'];
-	   $query = mysql_query("update catalist set title='$title' where cid='$id'");
-	   if($query){
-		   echo '1';
-	   }else{
-		   echo '2';
-	   }
-	   break;
-	case '':
-	   break;
+        $title = htmlspecialchars($_POST['title']);
+        $query = mysql_query("insert into catalist (cid,title) values (NULL,'$title')");
+        if ($query) {
+            $insertid = mysql_insert_id($link);
+            $arr = array('id' => $insertid, 'title' => $title, 'success' => 1);
+        } else {
+            $arr = array('success' => 2);
+        }
+        echo json_encode($arr);
+        break;
+    case 'del':  //删除项
+        $id = $_POST['id'];
+        $query = mysql_query("delete from catalist where cid=" . $id);
+        if ($query) {
+            echo '1';
+        } else {
+            echo '2';
+        }
+        break;
+    case 'edit':  //编辑项
+        $id = $_POST['id'];
+        $title = $_POST['title'];
+        $query = mysql_query("update catalist set title='$title' where cid='$id'");
+        if ($query) {
+            echo '1';
+        } else {
+            echo '2';
+        }
+        break;
+    case '':
+        break;
 }
-
-
 
 
 ?>
